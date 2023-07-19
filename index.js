7b25df634e36a15df9116df36e597603d2e129a2
preciousnjukia <preciousnjukia@gmail.com>
2023-07-19 19:38:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.Id = 'victory';
+
+const header = document.createElement('h1');
+newHeader.id = 'victory';
+
+const heading = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Precious is the champion'
